60c1325a8ad2f73826b8855390d4557bd5dc29d3
cceddie <crazycousineddie@gmail.com>
2012-01-30 12:13:38 +0100

    upload

--- cal-year.sh
+++ cal-year.sh
@@ -8,26 +8,33 @@
 # 
 # -----------------------------------------------------------------------------
 # Author/Info
+#
 # T. Charles Yun
-# tcharlesyun // hotmail // com
-#   with the above info, you should be able to use Google to get access to 
-#   email addresses that I use more frequently
 # License
 #   released GPL version 2 for those silly enough to want this
 #   that said, I do not have a copy of the license handy...
 # 
 # -----------------------------------------------------------------------------
 # todo:
-# - highlight curr month name? 
 # - i have had more than one day in which the output highlighting broke.  just 
 #   tried to check the code today (hard coding the dates on which errors 
 #   occured) and things work fine.  not sure what was wrong prev.  odd...
-#  
+#   - the 2011 may update should fix this prob.  remember to remove it...
+#
 # -----------------------------------------------------------------------------
 # Updates
 #
-# 2011 Jun
-# - cce cleanup
+# 2011 may 15
+# - i write some temp files for the months.  i clean them up now
+#
+# 2011 may 13
+# - updated the code to (hopefully) be a bit more smart.  
+#   - write each month individ to file
+#   - pad the end of each month with spaces and then crop using colrm
+#     which should get rid of the every once in a while odd output
+#   - use the "lam" function to assemble the months into columns
+#   - query the specific month to highlight a day
+# - the month is now also highlighted
 #
 # 2009 Mar 25
 # - added conditional so that date is only highlighted for current year
@@ -40,97 +47,7 @@
 # 2008 Jun
 # - created first pass
 #
-# -----------------------------------------------------------------------------
-# defaults and pre-calcs
-
-year=`date +%Y `
-today_year=`date +%Y `
-today_month=`date +%m | sed -E 's/^0//'  `
-today_date=`date +%d | sed -E 's/^0/ /'  | sed -E 's/$/ /'  ` 
-
-num_cols=4
-num_rows=3		
-
-row_month=1
-
-#ornament_l="\033[1m*"
-#ornament_r="*\033[0m"
-
-ornament_l="\033[1;36m*"
-ornament_r="*\033[0m"
-
-#ornament_l="*"
-#ornament_r="*"
-	# note: ornament left/right 
-	# but the idea is that it could be used to force the term to 
-	# change colors.  if this is done, i assume one would need a full
-	# section to determine the operating system and then the term type,
-	# avail colors, etc.
-	# 
-	#  "\[\033[1;36m\]"
-	#  "\[\033[0m\]"
-
-
-# -----------------------------------------------------------------------------
-check_variables() {
-
-	# check year 
-	# - is it set
-	# - is it an integer
-	# check col/width
-	# - are they set
-	# - do they multiply to 12
-	
-	if [[ $1 ]]
-	then
-		if [[ `echo "$1" | grep -E ^[[:digit:]]\{1,\}$` ]]
-		then
-			year=$1
-
-			if [[ $2 ]]
-			then
-				if [[ ! `echo "$2" | grep -E ^[[:digit:]]\{1,\}$` ]] 
-				then
-					# sigh, i was getting tired of embedded if/thens
-					# so this just checks if $2 is a digit
-					# if $3 is not a digit, i think that the
-					# multiplication check below will flag it.
-					
-					mcUsage
-				fi
-				
-				if [[ ! $3 ]]
-				then
-					mcUsage
-				fi
-				
-				if [[ $(( $2 * $3 )) -ne 12 ]] 
-				then
-					mcUsage
-				else
-					num_cols=$2
-					num_rows=$3		
-					makeCalendar
-				fi
-			else
-				# use all defaults as set above
-				makeCalendar
-			fi
-
-
-
-			makeCalendar
-	
-		else
-			mcUsage
-		fi
-	else
-		# use all defaults as set above
-		makeCalendar
-	fi
-
-}
-
+# 
 # -----------------------------------------------------------------------------
 # usage
 
@@ -141,14 +58,13 @@ mcUsage() {
 	echo "    a script that modifies the output of calto print a year's calendar"
 	echo "    with arbitrary numbers of columns and rows."
 	echo "Usage:"
-	echo "    year-cal [year [ [[number of cols] [numer of rows]]]]"
+	echo "    year-cal [year [number of cols]]"
 	echo ""
 	echo "    If no year is provided, the current year is calculated via the date"
-	echo "    command.  If a year is provided, the number of columns and rows can"
-	echo "    be provided, both are required and the product (row x col) must"
-	echo "    equal 12."
+	echo "    command.  If a year is provided, the number of columns can be"
+	echo "    provided."
 	echo "Example:"
-	echo "    yc 2008 4 3"
+	echo "    yc 2008 4"
 	echo "    would return the 2008 calendar, with four months in columns"
 	echo "    and three months of rows.  Briefly as below:"
 	echo "    "
@@ -157,85 +73,206 @@ mcUsage() {
 	echo "        may jun jul aug"
 	echo "        sep oct nov dec"
 	echo "    "
+	echo "    yc 2008 7"
+	echo "    would return the 2008 calendar with 7 columns.  The columns do"
+	echo "    not have to line up evenly at the end."
+	echo "        jan feb mar apr may jun jul"
+	echo "        aug sep oct nov dec"
+	
 	
 	exit
 
 }
 
-# -----------------------------------------------------------------------------
-makeCalendar() {		
-	while [[ $row_month -le 12 ]]
-		do
-			for row_week in 1 2 3 4 5 6 7 8
-			do
-				col_month=0
-				while [[ $col_month -lt $num_cols  ]]
-				do				
-					curr_month=$(( $row_month + $col_month ))
-					
-					month_line=`cal $curr_month $year | head -$row_week | tail -1`
-					
-					# must prepend a space so that the first column dates can get an asterisk
-					month_line=" $month_line"
-					
-					while [ ${#month_line} -le 21 ]
-					do
-						month_line="$month_line "
-					done
-					
-					
-					
-					if [[ $curr_month == $today_month ]] && [[ `echo $month_line | grep $today_date ` ]]
-					then
-						# ok, this is really sloppy, but i am sed'ing for "spaceDATEspace" and
-						# if the date is on sunday, then there is no leading space.
-						# so i am creating an exception IF/THEN statement to 
-						# deal with it.  I suspect that there are better/correct/elegant
-						# ways to do this...
-						
-						# echo ""
-						# echo "cm:x$curr_month x$today_month x"
-                                                # 
-						# echo "ml:x$month_line x"
-						# echo "td:x$today_date x"
-                                                # 
-							
-						if [[ `echo $month_line | grep "$today_date"` ]] && [[ $year == $today_year ]]
-						then
-							today_date=`echo "$today_date" | sed -E 's/^ //' | sed -E 's/ $//' `
-							month_line=$(echo "$month_line" | sed "s/ $today_date /`echo -e $ornament_l$today_date$ornament_r`/" )
-							#month_line=$(echo "$month_line" | sed "s/$today_date /`echo -e "$ornament_l""$today_date""$ornament_r"`/" )
-						else
-							month_line=$(echo "$month_line" | sed "s/ $today_date /`echo -e $ornament_l$today_date$ornament_r`/" )
-						fi
-					fi
-					
-					
-					full_line=$full_line$month_line 
-					
-					col_month=$(( $col_month + 1 ))
-				done
-				
-				echo -e "$full_line"
-				# i do not think this is needed, but it might be a good 
-				# to have just in case.  it re-sets the screen back 
-				# to a default state
-				# tput sgr0
-				
-				full_line=""
-				
-			done
-		row_month=$(( $row_month + $num_cols ))
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# all of the functions
+
+
+# set up variables
+	mainPath="/Users/tcyun/tcy-script/year-calendar/"
+
+	pass1=$1
+	pass2=$2
+	
+	width=4
+
+	currYear=`date +%Y `
+	currMonth=`date +%m `
+	currDate=`date +%d `
+	currHour=`date +%H `
+	currMins=`date +%M `
+	currSecs=`date +%S `
+	
+	month=1
+	limit=13
+	dumpLine=""
+	
+	#ornament left and right
+	# this first set uses color
+	#oL="\033[1;36m*"
+	oL="\033[31m>\033[34m"
+	oR="\033[31m<\033[0m"
+	# the below uses just plain text
+	#oL="*"
+	#oR="*"
+	
+	#month ornament left and right
+	# because the month does not have the asterisk
+	mL="\033[1;36m"
+	mR="\033[0m"
+	# or you can set this to nothing
+	#mL=""
+	#mR=""
+	
+checkParams () {
+	if [[ -n "$pass1" ]] 
+	then
+		if [[ "$pass1" -gt 0 ]] 
+		then
+			# if year is passed, use passed year
+			currYear=$pass1
+		else
+			mcUsage
+		fi
+		
+		# and then check to see if width was passed
+		if [[ -n $pass2 ]] && [[ $pass2 -gt 0 ]]
+		then
+			width=$pass2
+		else
+			# default width is 4
+			width=4
+		fi
+	else
+		# if year is not passed as parameter, then use curr year
+		currYear=`date +%Y`
+	fi	
+}
+
+buildMonthFiles () {
+	while [ "$month" -lt "$limit" ]
+	do
+		# so the purpose of the seds....
+		# first, add a space to the beginning of the line so that a highlight 
+		# marker can be inserted.
+		# second, add a full month of spaces to the end of the line so that 
+		# non-full weeks get space padded out for the full width
+		# last, column remove all the extra spaces so that the month has the 
+		# proper number of spaces to be laminated next to one another.
+		
+		cal $month $currYear | sed 's/^/\ /' | sed 's/$/                          /' | colrm 23 > "$mainPath"month$month
+		
+		month=`expr $month + 1`   
 		
 	done
 
+	month=1	
 }
 
-# -----------------------------------------------------------------------------
-# start by running the check variables procedure
-# that procedure will then call the calendar procedure
+renderCalendar () {
+	while [ "$month" -lt 12 ]
+	do
+		count=0
+		while [ "$count" -lt "$width" ]
+		do
+			if [ "$month" -lt 13 ]
+			then
+				dumpLine="$dumpLine "$mainPath"month$month"
+			fi
+			# echo -n "$count  "
+			month=`expr $month + 1 `
+			count=`expr $count + 1 `
+		done
+		
+		#echo $dumpLine
+		`echo "lam $dumpLine"`
+		dumpLine=""
+	done
+	
+	
+}
+
+highlightDate () {
+	
+	if [[ $currYear -eq `date +%Y ` ]] 
+	then
+		month=`expr $currMonth + 0  `
+	
+		# prepare date string 
+		# first, add zero to turn leading zero number into non leading zero
+		# then, take string length so that if it is single digit, you add a 
+		# leading space
+		# then add leading and trailing spaces so that you can get the prper
+		# match at the end
+		
+		date=`expr $currDate + 0  `
+		if [[ ${#date} -eq 1 ]]
+		then 
+			date=" $date"
+		fi
+		
+		# copy the specific month to a temp file
+		# manipulate the file by line
+		# write the output of the manipulation to the orig file name
+		
+		mv    "$mainPath"month$month "$mainPath"monthTemp$month
+		touch "$mainPath"month$month
+	
+		# now, take the selected month
+		# pull apart by line
+		# grep check each line
+		# sed the line
+		
+		line=8
+		
+		while [ $line -gt 0 ]
+		do 
+			week=`tail -"$line" "$mainPath"monthTemp$month | head -1 `
+			if [[ $line -eq 8 ]]
+			then
+				echo -e "$week" | sed -E "s/^/`echo -e "$mL"`/" | sed -E "s/$/`echo -e "$mR"`/" >>"$mainPath"month$month			
+			elif [[ `echo "$week" | grep "$date"` ]]
+			then
+				echo -e "$week" | sed -E "s/ $date /`echo -e "$oL$date$oR"`/" >>"$mainPath"month$month
+			else
+				echo "$week" >>"$mainPath"month$month
+			fi
+			line=$(( $line - 1 ))
+		done	
+		
+		month=1
+	fi
+	
+}
+
+doNothing () {
+	# this is odd
+	echo 
+}
+
+cleanUp () {
+	cd $mainPath
+	
+	for a in {1..12}
+	do
+		rm month$a
+		rm monthTemp$a &> /dev/null
+	done
+	
+}
 
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# ok, run everything form here
 
-check_variables $1 $2 $3
+checkParams
+buildMonthFiles
+highlightDate
+renderCalendar
+cleanUp
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# and we are done
+
+exit
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
